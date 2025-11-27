73da81c269ab9dacd28c49ec5540cb2281ddc2cc
Ali Rammal <alirammal@Alis-MacBook-Air.local>
2025-11-27 19:34:54 +0200

    Update Twitter redirect URL to new tweet

--- lib/twitter.ts
+++ lib/twitter.ts
@@ -1,2 +1,2 @@
-export const TWITTER_REDIRECT_URL = "https://x.com/MaestroBots/status/1978513825784635852";
+export const TWITTER_REDIRECT_URL = "https://x.com/MaestroBots/status/1994096398837362765?s=20";
 
